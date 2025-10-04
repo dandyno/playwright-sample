3363b9a3d9b7b7cd423b5ccc6e0d24f8514197ec
dandyno <dandynorbert8@gmail.com>
2025-10-04 13:40:59 +0700

    update files
    update loginPage and StorePageObjects

---
+++ LoginPage.ts
@@ -0,0 +1,40 @@
+import { LoginPageObject } from '../objects/LoginPageObjects';
+import { Page } from 'playwright';
+
+export default class LoginPage {    
+    readonly page: Page;
+    constructor(page: Page) {
+        this.page = page;
+    }
+
+    async open() {
+        await this.page.goto('https://www.saucedemo.com/');
+    }
+
+    async login(username: string, password: string) {
+        await this.page.fill(LoginPageObject.usernameField, username);
+        await this.page.fill(LoginPageObject.passwordField, password);
+        await this.page.click(LoginPageObject.loginButton);
+    }
+
+    async IsStorePageVisible() {
+        await this.page.waitForSelector(LoginPageObject.inventoryList);
+        return await this.page.isVisible(LoginPageObject.inventoryList);
+    }
+
+    async IsLoginPageVisible() {
+        try {
+            await this.page.waitForSelector(LoginPageObject.usernameField);
+            return await this.page.isVisible(LoginPageObject.usernameField); 
+        } catch (error) {
+            console.log("Error in IsLoginPageVisible:", error);
+            return false;
+        }
+    }
+
+    async assertErrorMessageVisible() {
+        const isErrorVisible = await this.page.isVisible(LoginPageObject.errorMessageContainer);
+        return isErrorVisible;
+    }
+
+}
